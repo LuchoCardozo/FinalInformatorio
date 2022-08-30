53c7b0578e02ee41064502e35bdb2d45a949d9d1
LuchoCardozo <you@example.com>
2022-08-30 18:55:58 -0300

    heroku6

---
+++ finalInfo/finalInfo/settings/local.py
@@ -0,0 +1,13 @@
+from .base import *
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'finalInfo_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'LOCALHOST':'3306',
+    }
+}
