f80d73040a1a024dbaa8a3e9df5aa0c394b003d9
LuchoCardozo <you@example.com>
2022-08-30 18:01:34 -0300

    arreglo de model

--- finalInfo/apps/noticias_app/models.py
+++ finalInfo/apps/noticias_app/models.py
@@ -9,6 +9,9 @@ from django.conf import settings
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre
 
 
 class Noticia(models.Model):
