c89cabc7d9cb8b6b5bb777f256af891e21568041
LuchoCardozo <you@example.com>
2022-08-30 21:08:14 -0300

    heroku21

--- finalInfo/settings/base.py
+++ finalInfo/settings/base.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure--+3r!-6&hod$+w3mhpb=-x%(kcja(_=()o31$-h$h9^o^iopt*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
